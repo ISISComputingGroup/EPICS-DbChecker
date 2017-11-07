6afc3c44ffbec7d738483bfde1c104f05517d5fb
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-11-07 10:40:45 +0000

    Some tidying up

--- db_parser.py
+++ db_parser.py
@@ -10,7 +10,9 @@ record_start_regex = 'record\s*\((\w+),\s*"([\w_\-\:\[\]<>;$\(\)]+)"\)'
 def parse_db(file):
     # Aliases look like alias("RECORDNAME","ALIASNAME")
     alias_regex = 'alias\s*\(\s*"([\w_\-\:\[\]<>;$\(\)]+)",\s*"([\w_\-\:\[\]<>;$\(\)]+)"\)'
-    
+
+    field_regex = '\s+field\s*\(([A-Z]+),\s*"([\w_\-\:\[\]<>;$\(\)]+)"\)'
+
     with open(file, 'r') as f:
 
         records = {}
@@ -18,56 +20,43 @@ def parse_db(file):
         record = None
 
         for line in f:
-            maPV = re.match(record_start_regex, line)
-            maAlias2 = re.match(alias_regex, line)
-
             if inrecord:
                 # Look for end of record
-                maEnd = re.match("}$", line.strip())
-                if not (maEnd is None):
+                matched_end = re.match("}$", line.strip())
+                if not (matched_end is None):
                     inrecord = False
                     records[record.name] = record
                 else:
-                    # Look for alias
-                    regAlias_inside = 'alias\s*\(\s*"([\w_\-\:\[\]<>;$\(\)]+)"\)'
-                    maAlias_inside = re.match(regAlias_inside, line)
-                    # Look for SIML
-                    regSiml = '\s+field\s*\(SIML,\s*"([\w_\-\:\[\]<>;$\(\)]+)"\)'
-                    maSiml = re.match(regSiml, line)
-                    # Look for SDIS
-                    regSdis = '\s+field\s*\(SDIS,\s*"([\w_\-\:\[\]<>;$\(\)]+)"\)'
-                    maSdis = re.match(regSdis, line)
-                    # Look for DTYP
-                    regDtyp = '\s+field\s*\(DTYP,\s*"([\w_\-\:\[\]<>;$\(\)\s]+)"\)'
-                    maDtyp = re.match(regDtyp, line)
-                    # Look for NELM
-                    regNelm = '\s+field\s*\(NELM,\s*"([\w_\-\:\[\]<>;$\(\)\s]+)"\)'
-                    maNelm = re.match(regNelm, line)
-                    # Look for FTVL
-                    regFtvl = '\s+field\s*\(FTVL,\s*"([\w_\-\:\[\]<>;$\(\)\s]+)"\)'
-                    maFtvl = re.match(regFtvl, line)
+                    # Look for alias inside a record
+                    inner_alias_regex = 'alias\s*\(\s*"([\w_\-\:\[\]<>;$\(\)]+)"\)'
+                    matched_inner_alias = re.match(inner_alias_regex, line)
+                    # Look for a field
+                    matched_field = re.match(field_regex, line)
 
-                    if not (maAlias_inside is None):
+                    if matched_inner_alias is not None:
                         # There is an alias
-                        record.alias = Alias(maAlias_inside.groups()[0], record.type, record.name)
-                    if not (maSiml is None):
-                        record.siml = maSiml.groups()[0]
-                    if not (maSdis is None):
-                        record.sdis = maSdis.groups()[0]
-                    if not (maDtyp is None):
-                        record.dtyp = maDtyp.groups()[0]
-                    if not (maNelm is None):
-                        record.nelm = maNelm.groups()[0]
-                    if not (maFtvl is None):
-                        record.ftvl = maFtvl.groups()[0]
+                        record.alias = Alias(matched_inner_alias.groups()[0], record.type, record.name)
+                    if matched_field is not None:
+                        if matched_field.groups()[0] == "SIML":
+                            record.siml = matched_field.groups()[1]
+                        if matched_field.groups()[0] == "SDIS":
+                            record.sdis = matched_field.groups()[1]
+                        if matched_field.groups()[0] == "DTYP":
+                            record.dtyp = matched_field.groups()[1]
+                        if matched_field.groups()[0] == "NELM":
+                            record.nelm = matched_field.groups()[1]
+                        if matched_field.groups()[0] == "FTVL":
+                            record.ftvl = matched_field.groups()[1]
             else:
-                if not (maPV is None):
+                matched_pv = re.match(record_start_regex, line)
+                matched_alias = re.match(alias_regex, line)
+                if matched_pv is not None:
                     # Found start of record
                     inrecord = True
-                    record = Record(maPV.groups()[1], maPV.groups()[0])
-                elif not (maAlias2 is None):
-                    alias = Alias(maAlias2.groups()[1], parent = maAlias2.groups()[0])
-                    records[maAlias2.groups()[1]] = alias
+                    record = Record(matched_pv.groups()[1], matched_pv.groups()[0])
+                elif matched_alias is not None:
+                    alias = Alias(matched_alias.groups()[1], parent=matched_alias.groups()[0])
+                    records[matched_alias.groups()[1]] = alias
     
     return records
     
