42796da512ae1398b4a3cb49b4ee9ec7afff7384
Sam Jenkins <s.jenkins@stfc.ac.uk>
2021-04-30 10:53:59 +0100

    added folders to ignore and folders to only run less strict checks

--- check_db_file.py
+++ check_db_file.py
@@ -3,6 +3,7 @@ import os
 import glob
 from src.db_checker import DbChecker
 from src.db_parser.common import DbSyntaxError
+
 DIRECTORIES_TO_ALWAYS_IGNORE = [
     ".git",
     "O.Common",
@@ -18,12 +19,165 @@ DIRECTORIES_TO_ALWAYS_IGNORE = [
     "DbChecker",  # tests that delibratly fail here
     "DbUnitChecker",  # tests that delibratly fail here
     "base",  # need to fix parsing of {} in fields before checking here
-    "ICP_Binaries"  # contains files that end in .db not in .db format
+    "ICP_Binaries",  # contains files that end in .db not in .db format
+    "seq",
+    "caLab_1505",
+    "EUROTHRM",
+]
+
+# For the stricter checks, we only care about dbs written by locally
+DIRECTORIES_TO_IGNORE_STRICT = [
+    "AG3631A",
+    "AG53220A",
+    "Agilent_53220A",
+    "aldn1000",
+    "ASTRIUM",
+    "asyn",
+    "autosave",
+    "axis",
+    "axisRecord",
+    "barndoors",
+    "BKHOFF",
+    "busy",
+    "CAENMCA",
+    "CAENVME",
+    "calc",
+    "CCD100",
+    "CONEXAGP",
+    "COUETTE",
+    "cryosms",
+    "CRYVALVE",
+    "CYBAMAN",
+    "DAQmxBase",
+    "dbVerbose_20130124",
+    "DELFTBPMAG",
+    "DELFTDCMAG",
+    "DELFTSHEAR",
+    "devIocStats",
+    "DFKPS",
+    "dh2000",
+    "dma4500m",
+    "dna4500m",
+    "ECLab",
+    "EGXCOLIM",
+    "ether_ip",
+    "ethercat",
+    "fb_epid",
+    "FERMCHOP",
+    "FileList",
+    "FileServer",
+    "FINS",
+    "flipprps",
+    "FMR",
+    "FZJDDFCH",
+    "GALIL",
+    "galil",
+    "GAMRY",
+    "gateway",
+    "GEMORC",
+    "googletest",
+    "GP2Camera",
+    "HAMEG8123",
+    "Hameg_8123",
+    "heliox",
+    "HIFIMAG",
+    "HIFIMAGS",
+    "hlx503",
+    "HVCAEN",
+    "HVCAENx527",
+    "IEG",
+    "ILM200",
+    "indfurn",
+    "INSTETC",
+    "INSTRON",
+    "iocbuilder-3-20",
+    "ipApp",
+    "IPS",
+    "isisdae",
+    "isisdaedata",
+    "itc503",
+    "jaws",
+    "jsco4180",
+    "JULABO",
+    "keithley_2001",
+    "kepco",
+    "keylkg",
+    "KHLY2700",
+    "knr1050",
+    "lakeshore340",
+    "lakeshore372",
+    "LINKAM95",
+    "LINMOT",
+    "LKSH336",
+    "LKSH460",
+    "lua",
+    "magnet3D",
+    "mca",
+    "MCAG_Base_Project",
+    "MCLEN",
+    "mercuryitc",
+    "mezflipr",
+    "motionSetPoints",
+    "motor",
+    "motorExtensions",
+    "moxa12XX",
+    "MSH150",
+    "MuonJaws",
+    "nanodac",
+    "NEOCERA",
+    "NetShrVar",
+    "NetStreams",
+    "nGEM-BBTX",
+    "ngpspsu",
+    "NIMATRO",
+    "NWPRTXPS",
+    "oercone",
+    "opticsApp",
+    "PIMOT",
+    "pixelman",
+    "pr4000",
+    "procServControl",
+    "ReadASCII",
+    "RKNPS",
+    "rotating_sample_changer",
+    "RotBench",
+    "RunControl",
+    "RUNCTRL",
+    "SAMPOS",
+    "separator",
+    "SKFChopper",
+    "SKFMB350",
+    "SM300",
+    "SMC100",
+    "sp2xx",
+    "SPINFLIPPER306015",
+    "sscan",
+    "std",
+    "StreamDevice",
+    "superlogics",
+    "TDK_LAMBDA_GENESYS",
+    "TEKDMM40X0",
+    "Tektronix_DMM_40X0",
+    "timestampRecord",
+    "TPG",
+    "TRITON",
+    "ttiEX355P",
+    "ttiplp",
+    "TWINCAT",
+    "utilities",
+    "utilitiesApp",
+    "VisualDCT",
+    "wbvalve",
+    "webget",
+    "WinDDE",
+    "wm323",
+    "zfcntrl"
 ]
 
 
-def check_files(db_files, verbose):
+def check_files(db_files, strict, verbose):
     total_errors = 0
+    total_strict_errors = 0
     total_warning = 0
     current_item = 1
     final_item = len(db_files)
@@ -34,23 +188,34 @@ def check_files(db_files, verbose):
             fp = os.path.abspath(f)
             dbc = DbChecker(fp, verbose)
             dbc.parse_db_file()
-            syntax_warnings, syntax_errors = dbc.syntax_check()
+            syntax_warnings = 0
+            syntax_errors = 0
+            if f in strict:
+                syntax_warnings, syntax_errors = dbc.syntax_check()
             pv_warnings, pv_errors = dbc.pv_check()
+            if syntax_errors > 0 or pv_errors > 0:
+                failed.append(f)
             total_warning += syntax_warnings + pv_warnings
             total_errors += syntax_errors + pv_errors
+            total_strict_errors += syntax_errors
         except DbSyntaxError as e:
             print(e)
             failed_to_parse.append(f)
         except IOError:
             print("FILE ERROR: File {} does not exist".format(f))
         finally:
-            print("{} Files remaining".format(final_item - current_item))
+            print("File {} of {}.".format(current_item, final_item))
             current_item += 1
     if final_item > 1:
         print("** TOTAL WARNING COUNT = {} **".format(total_warning))
         print("** TOTAL ERROR COUNT = {} **".format(total_errors))
-    if len(failed_to_parse)>0:
+        print("** TOTAL STRICT ERROR COUNT = {} **".format(total_strict_errors))
+    if len(failed_to_parse) > 0:
         print("Failed to parse the following files: \n{}".format(failed_to_parse))
+    if len(failed) > 0:
+        print("The following files failed the checks: \n")
+        for f in failed:
+            print(f)
 
 
 if __name__ == '__main__':
@@ -79,12 +244,18 @@ if __name__ == '__main__':
         if len(args.directory) > 0:
             if args.recursive:
                 to_check = []
+                strict_check = []
                 for root, dirs, files in os.walk(args.directory[0]):
                     dirs[:] = [d for d in dirs if d not in DIRECTORIES_TO_ALWAYS_IGNORE]
                     for file in files:
                         if file.endswith(".db"):
                             to_check.append(os.path.join(root, file))
-                check_files(to_check, args.verbose)
+                for root, dirs, files in os.walk(args.directory[0]):
+                    dirs[:] = [d for d in dirs if d not in DIRECTORIES_TO_IGNORE_STRICT]
+                    for file in files:
+                        if file.endswith(".db"):
+                            strict_check.append(os.path.join(root, file))
+                check_files(to_check, strict_check, args.verbose)
             else:
                 # Find db files in directory
                 os.chdir(args.directory[0])
