06acf4dc50d34e30ae687baf5247dafdd85e1a1e
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-03-13 22:53:35 +0000

    Exclude Lakeshore340.db

--- check_db_file.py
+++ check_db_file.py
@@ -56,6 +56,7 @@ DIRECTORIES_TO_ALWAYS_IGNORE = [
     "runcontrol.db",  # Uses EPICS 7 JSON syntax which is not yet supported in the parser
     "settings_xml.db",  # Uses EPICS 7 JSON syntax which is not yet supported in the parser
     "testParseJSON.db",  # Uses EPICS 7 JSON syntax which is not yet supported in the parser
+    "Lakeshore340.db",  # Uses EPICS 7 JSON syntax which is not yet supported in the parser
 ]
 
 # For the stricter checks, we only care about dbs written by ISIS
